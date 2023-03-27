33be2cbafe098aa4e94bd4690ae6abb97cd733d5
JMinio <juli.mno18@gmail.com>
2023-03-27 11:11:31 -0300

    creando dashboard

---
+++ main.js
@@ -0,0 +1,15 @@
+
+const dashboard = document.getElementById("dashboard");
+const mostrar = document.getElementById("mostrar");
+
+/* import data from './data.json';
+ */
+
+mostrar.addEventListener("click", ()=> {
+    dashboard.innerHTML += `
+    
+    
+
+    `
+
+})
